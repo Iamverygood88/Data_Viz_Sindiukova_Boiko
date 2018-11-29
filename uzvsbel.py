418e2ff6b1339b494dcc97862f59eecd9b3057ee
Евгения Синдюкова <evgeniia@Evgenias-MBP.fc.ca>
2018-11-29 11:53:31 -0500

    my part

--- uzvsbel.py
+++ uzvsbel.py
@@ -1,10 +1,11 @@
-import numpy as np 
+import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
 categories = []
-Ukraine = []
+Uzbekistan = []
 Belarus = []
+
 
 with open('data/OlympicsWinter.csv') as csvfile:
     reader = csv.reader(csvfile)
@@ -13,34 +14,37 @@ with open('data/OlympicsWinter.csv') as csvfile:
     for row in reader:
         if line_count is 0:
             categories.append(row)
-            line_count += 1  
+            line_count += 1
+
         else:
-            if row[4] == 'UKR':
-                print('total medals for Ukraine:', len(Ukraine))
-                Ukraine.append([int(row[0]), row[5], row[6], row[7]])
-            elif row[4] == 'BLR':
+            if row[4] == "UZB":
+                print('total medals for Uzbekistan:', len(Uzbekistan))
+                Uzbekistan.append([int(row[0]), row[5], row[6], row[7]])
+            elif row[4] == "BLR":
                 print('total medals for Belarus:', len(Belarus))
                 Belarus.append([int(row[0]), row[5], row[6], row[7]])
                 line_count += 1
 
-totalMedals = len(Ukraine) + len(Belarus)
+totalMedals = len(Uzbekistan) + len(Belarus)
 
-Ukraine_procentage = int(len(Ukraine) / totalMedals * 100)
+Uzbekistan_procentage = int(len(Uzbekistan) / totalMedals * 100)
 Belarus_procentage = int(len(Belarus) / totalMedals * 100)
 
 print('processed', line_count, 'line of data.Total medals', totalMedals)
 
-x2 = ['Ukraine']
-y2 = [Ukraine_procentage]
+x = ['Belarus']
+y = [Belarus_procentage]
 
-x3 = ['Belarus']
-y3 = [Belarus_procentage]
+x2 = ['Uzbekistan']
+y2 = [Uzbekistan_procentage]
 
-plt.bar(x2, y2, label='Ukraine', color='tomato')
-plt.bar(x3, y3, label='Belarus', color='darkorange')
+
+plt.bar(x, y, label='Belarus', color='coral')
+plt.bar(x2, y2, label='Uzbekistan', color='crimson')
 
 plt.xlabel('Countries')
 plt.ylabel('Amount of Medals')
-plt.title('Belarus VS Ukraine')
+
+plt.title('Belarus VS Uzbekistan')
 plt.legend()
 plt.show()
